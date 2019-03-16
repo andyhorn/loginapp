ed90c6e7303e79dfeb68faaf0583f54325e95849
= <=>
2019-03-15 21:42:43 -0700

    Registration complete

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express     = require('express');
 var router      = express.Router();
+var User        = require('../models/user')
 
 // Register
 router.get('/register', (req, res) => {
@@ -11,5 +12,45 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// Register user
+router.post('/register', (req, res) => {
+    let name = req.body.name,
+    email = req.body.email,
+    username = req.body.username,
+    password = req.body.password,
+    password2 = req.body.password2;
+
+    // Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        console.log('ERRORS');
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        console.log('No errors');
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+        User.createUser(newUser, (err, user) => {
+            if (err) throw err;
+            console.log(user);
+        });
+
+        req.flash('success_msg', 'You are registered and can now login');
+        res.redirect('/users/login');
+    }
+});
 
 module.exports = router;
